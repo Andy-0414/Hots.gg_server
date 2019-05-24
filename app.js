fdddb211e9eadac6c72432b57bdd7f5af1c8f15d
Andy0414 <pjh8667@naver.com>
2019-05-24 10:51:55 +0900

    UPDATE INVEN

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/',(req,res)=>{
     res.sendFile('./public/index.html')
 })
 app.get('/data',(req,res)=>{
+    console.log(hosCrawler.getHeroDataList())
     res.send(hosCrawler.getHeroDataList())
 })
 app.listen(3000,()=>{
--- modules/hos-crawler.js
+++ modules/hos-crawler.js
@@ -1,9 +1,10 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
 
-class HosCrawler{
-    constructor(){
-        this.heroDataList = [];
+class HosCrawler {
+    constructor() {
+        this.heroDataList = []
+        this.invenDataList = []
         this.update()
     }
     update() {
@@ -12,19 +13,33 @@ class HosCrawler{
                 .then(html => {
                     var $ = cheerio.load(html.data)
                     var str = $("script").get()[9].children[0].data
-    
+
                     this.heroDataList = JSON.parse(str.split(/(window.blizzard.hgs.*) =/g)[2].replace(";", ""))
                     resolve(this.heroDataList)
                 })
                 .catch(err => [
                     reject(err)
                 ])
+            axios.get("http://hos.inven.co.kr/dataninfo/hero/")
+                .then(html => {
+                    var $ = cheerio.load(html.data)
+                    var heroCodeList = $(".hosDbCommonHeroList").children()
+                    this.invenDataList = []
+                    heroCodeList.each((i, e) => {
+                        if (i == 0) return
+                        this.invenDataList.push({
+                            name: heroCodeList[i].children[1].children[0].data,
+                            code: e.attribs.href.split('=')[1],
+                        })
+                    })
+                    console.log(this.invenDataList)
+                })
         })
     }
-    getHeroData(key){
-        return this.heroDataList.find(x=>{x.name == key})
+    getHeroData(key) {
+        return this.heroDataList.find(x => { x.name == key })
     }
-    getHeroDataList(){
+    getHeroDataList() {
         return this.heroDataList
     }
 }
