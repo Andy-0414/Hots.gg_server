533f29866b0504d53aeac414d77cb410961dacac
Andy0414 <pjh8667@naver.com>
2019-05-24 12:14:14 +0900

    UPDATE promise

--- modules/hos-crawler.js
+++ modules/hos-crawler.js
@@ -5,62 +5,69 @@ class HosCrawler {
     constructor() {
         this.heroDataList = []
         this.invenDataList = []
-        this.update()
+        this.update().then(data => { console.log("CLEAR") })
     }
     update() {
-        return new Promise((resolve, reject) => {
-            axios.get("https://heroesofthestorm.com/ko-kr/heroes/")
-                .then(html => {
-                    var $ = cheerio.load(html.data)
-                    var str = $("script").get()[9].children[0].data
+        return Promise.all([
+            new Promise((resolve, reject) => {
+                axios.get("https://heroesofthestorm.com/ko-kr/heroes/")
+                    .then(html => {
+                        var $ = cheerio.load(html.data)
+                        var str = $("script").get()[9].children[0].data
 
-                    this.heroDataList = JSON.parse(str.split(/(window.blizzard.hgs.*) =/g)[2].replace(";", ""))
-                    resolve(this.heroDataList)
-                })
-                .catch(err => [
-                    reject(err)
-                ])
-            axios.get("http://hos.inven.co.kr/dataninfo/hero/")
-                .then(html => {
-                    var $ = cheerio.load(html.data)
-                    var heroCodeList = $(".hosDbCommonHeroList").children()
-                    this.invenDataList = []
-                    var promiseList = []
-                    heroCodeList.each((i, e) => {
-                        if (i == 0) return
-                        var name = heroCodeList[i].children[1].children[0].data
-                        var code = e.attribs.href.split('=')[1]
-                        this.invenDataList.push({
-                            name,
-                            code,
-                            Ability: []
-                        })
-
-                        promiseList.push(new Promise(resolve => {
-                            axios.get(`http://hos.inven.co.kr/dataninfo/hero/detail.php?code=${code}`)
-                                .then(html => {
-                                    var $ = cheerio.load(html.data)
-                                    var Ability = $(".talentLevel0>dd")
-                                    var Ability_TMP = []
-                                    Ability.children().each((i, e) => {
-                                        var current = $(e).find("p.name")
-                                        Ability_TMP.push({
-                                            name : current.text(),
-                                            text : current.next().text(),
-                                            tier : e.attribs["data-talent-tier"]
+                        this.heroDataList = JSON.parse(str.split(/(window.blizzard.hgs.*) =/g)[2].replace(";", ""))
+                        resolve(this.heroDataList)
+                    })
+                    .catch(err => [
+                        reject(err)
+                    ])
+            }),
+            new Promise((resolve, reject) => {
+                axios.get("http://hos.inven.co.kr/dataninfo/hero/")
+                    .then(html => {
+                        var $ = cheerio.load(html.data)
+                        var heroCodeList = $(".hosDbCommonHeroList").children()
+                        this.invenDataList = []
+                        var promiseList = []
+                        heroCodeList.each((i, e) => {
+                            if (i == 0) return
+                            var name = heroCodeList[i].children[1].children[0].data
+                            var code = e.attribs.href.split('=')[1]
+                            this.invenDataList.push({
+                                name,
+                                code,
+                                Ability: []
+                            })
+                            promiseList.push(new Promise(resolve => {
+                                axios.get(`http://hos.inven.co.kr/dataninfo/hero/detail.php?code=${code}`)
+                                    .then(html => {
+                                        var $ = cheerio.load(html.data)
+                                        var Ability = $(".talentLevel0>dd")
+                                        var Ability_TMP = []
+                                        Ability.children().each((i, e) => {
+                                            var current = $(e).find("p.name")
+                                            Ability_TMP.push({
+                                                name: current.text(),
+                                                text: current.next().text(),
+                                                tier: e.attribs["data-talent-tier"]
+                                            })
+                                            this.invenDataList[this.invenDataList.findIndex(x => x.name == name)].Ability = Ability_TMP
                                         })
-                                        this.invenDataList[this.invenDataList.findIndex(x=>x.name == name)].Ability = Ability_TMP
+                                        resolve(html.data)
                                     })
-                                    resolve(html.data)
-                                })
-                        }))
+                            }))
 
+                        })
+                        Promise.all(promiseList)
+                            .then(data => {
+                                resolve(data)
+                            })
+                            .catch(err => {
+                                reject(err)
+                            })
                     })
-                    Promise.all(promiseList).then(data => {
-                        console.log(this.invenDataList)
-                    })
-                })
-        })
+            })])
+
     }
     getHeroData(key) {
         return this.heroDataList.find(x => { x.name == key })
