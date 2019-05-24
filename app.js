9b1188b44dedd14b9ff91b127c122ecabef631f1
Andy0414 <pjh8667@naver.com>
2019-05-24 09:11:32 +0900

    UPDATE function

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-const getHeroData = () => {
-    return axios.get("https://api.hotslogs.com/Public/Data/Heroes")
-}
-const getOneHeroFullPage = (key) => {
-    return axios.get(`https://heroesofthestorm.com/ko-kr/heroes/${key}/`)
-}
-const getHeroListPage = () => {
-    return axios.get("https://heroesofthestorm.com/ko-kr/heroes/")
-}
-getHeroListPage()
-    .then(html => {
-        const $ = cheerio.load(html.data)
-        const str = $("script").get()[9].children[0].data
-        console.log(JSON.parse(str.split(/(window.blizzard.hgs.*) =/g)[2].replace(";", ""))[0])
+const express = require('express')
+const app = express()
+
+const axios = require("axios")
+const cheerio = require("cheerio")
+
+var heroInfomationList = []
+
+function getHeroData() {
+    return new Promise((resolve, reject) => {
+        axios.get("https://heroesofthestorm.com/ko-kr/heroes/")
+            .then(html => {
+                var $ = cheerio.load(html.data)
+                var str = $("script").get()[9].children[0].data
+
+                heroInfomationList = JSON.parse(str.split(/(window.blizzard.hgs.*) =/g)[2].replace(";", ""))
+                resolve(heroInfomationList)
+            })
+            .catch(err => [
+                reject(err)
+            ])
     })
-// getHeroData()
-//     .then(data => {
-//         data.data.forEach(x => {
-//             getOneHeroFullPage(x.Translations.split(",")[0].toLowerCase())
-//                 .then(html => {
-//                     const $ = cheerio.load(html.data)
-//                     var text = $('.hero-identity').find(".hero-identity__name").text()
-//                     console.log(text)
-//                 })
-//                 .catch(err=>{
-//                     console.log(x.Translations.split(",")[0], "ERR")
-//                 })
-//         })
-//     })
+}
+getHeroData()
+app.get('/',(req,res)=>{
+    res.sendFile('./public/index.html')
+})
+app.get('/data',(req,res)=>{
+    res.send(heroInfomationList)
+})
+app.listen(3000,()=>{
+    console.log("SERVER OPEN")
+})
