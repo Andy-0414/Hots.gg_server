0b1a87b0039c5aab0f026658a3b2a3b2f68499d1
Andy0414 <pjh8667@naver.com>
2019-06-11 08:42:15 +0900

    UPDATE test

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express()
 
 const hosCrawler = require('./modules/hos-crawler')
 
-app.use(express.static("public"))
+app.use(express.static("public")) // ./public 정적 파일 제공
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // DEBUG 페이지
     res.sendFile('./public/index.html')
 })
-app.get('/getHeroData', (req, res) => {
+app.get('/getHeroData', (req, res) => { // 영웅 정보 불러오기
     res.send(hosCrawler.getHeroDataList())
 })
-app.listen(3000, () => {
+app.listen(3000, () => { // 3000포트
     console.log("SERVER OPEN")
 })
